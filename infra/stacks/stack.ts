e1aff7ddc878758dd7d1047243b6e8682bb642f8
Promie Yutasane <promie.yutasane@gmail.com>
2025-12-04 20:24:17 +1100

    apply app wide prettier write changes

--- infra/stacks/stack.ts
+++ infra/stacks/stack.ts
@@ -2,16 +2,20 @@ import { Stack, StackProps } from 'aws-cdk-lib'
 import { Construct } from 'constructs'
 
 export type CloudETLStepFunctionsStackProps = StackProps & {
-    appName: string
-    stage: string
+  appName: string
+  stage: string
 }
 
 export class CloudETLStepFunctionsStack extends Stack {
-    public readonly apiUrl: string
+  public readonly apiUrl: string
 
-    constructor(scope: Construct, id: string, props: CloudETLStepFunctionsStackProps) {
-        super(scope, id, props)
+  constructor(
+    scope: Construct,
+    id: string,
+    props: CloudETLStepFunctionsStackProps,
+  ) {
+    super(scope, id, props)
 
-        const { appName, stage } = props
-    }
+    const { appName, stage } = props
+  }
 }
